5cd99471becaa498de72cd671c9b7a288374ea35
Anirudh Nuti <NvkAnirudh@users.noreply.huggingface.co>
2024-09-24 07:40:28 +0000

    Update src/google_docs_api_processing.py

--- src/google_docs_api_processing.py
+++ src/google_docs_api_processing.py
@@ -23,7 +23,7 @@ def google_docs_auth():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES
             )
-            creds = flow.run_local_server(port=8502)
+            creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
